841ed3d6cab347c9b9159b88b0d78fe88a0e4139
mdonit <donitwod@gmail.com>
2024-07-29 21:58:41 +0200

    cinematic animations: day and night transitions

--- Game/scripts/cinematics/loadingScreen.gd
+++ Game/scripts/cinematics/loadingScreen.gd
@@ -5,6 +5,7 @@ class_name LoadingScreen
 @export var fadeOutTime:float = 1.0
 @export var minTimeOnScreen:float = 1.0
 @export var content:CanvasItem
+@export var isItDayToNight: bool = true
 
 enum Mode {FadeIn, Visible, FadeOut}
 var mode:Mode = Mode.FadeIn
@@ -12,9 +13,17 @@ var mode:Mode = Mode.FadeIn
 signal onFadeInFinished
 signal onFadeOutFinished
 
+var fromSprite: Sprite2D
+var toSprite: Sprite2D
+var animationPlayer: AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	content.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	fromSprite = $CanvasLayer/Panel/DayNightSpritesPanel/DayNightSpritesNode/FromSprite
+	toSprite = $CanvasLayer/Panel/DayNightSpritesPanel/DayNightSpritesNode/ToSprite
+	animationPlayer = $CanvasLayer/Panel/DayNightSpritesPanel/DayNightSpritesNode/AnimationPlayer
+	toSprite.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,6 +31,7 @@ func _process(delta):
 		Mode.FadeIn:
 			content.modulate.a += delta / fadeInTime
 			if (content.modulate.a >= 1.0):
+				dayNightAnimation()
 				onFadeInFinished.emit()
 				mode = Mode.Visible
 	
@@ -30,3 +40,11 @@ func _process(delta):
 			if (content.modulate.a <= 0.0):
 				onFadeOutFinished.emit()
 				queue_free()
+
+func dayNightAnimation():
+	toSprite.visible = true
+	animationPlayer.play("day_to_night_animation")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "day_to_night_animation":
+		fromSprite.visible = false
