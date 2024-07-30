3ebbd76dcff6f3f5a83a265c039bfd52c270bd55
mdonit <donitwod@gmail.com>
2024-07-30 13:33:32 +0200

    Animation: Game Over Screen + Day-Night Anim fix

--- Game/scripts/cinematics/loadingScreen.gd
+++ Game/scripts/cinematics/loadingScreen.gd
@@ -6,7 +6,11 @@ class_name LoadingScreen
 @export var minTimeOnScreen:float = 1.0
 @export var content:CanvasItem
 @export var isItDayToNight: bool = true
+
 @export var isItGameOver: bool = false
+var gameOverAnimFinished: bool = false
+var bloodSprites1: Node2D
+var bloodSprites2: Node2D
 
 enum Mode {FadeIn, Visible, FadeOut}
 var mode:Mode = Mode.FadeIn
@@ -29,6 +33,9 @@ func _ready():
 	toSprite.visible = false
 	if isItGameOver:
 		fromSprite.visible = false
+		bloodSprites1 = $CanvasLayer/GameOverPanel/GameOverSprites/BloodSpritesNode01
+		bloodSprites2 = $CanvasLayer/GameOverPanel/GameOverSprites/BloodSpritesNode02
+		gameOverAnimation()
 	
 	var audioPlayer = AudioStreamPlayer.new()
 	audioPlayer.stream = sfx
@@ -39,25 +46,37 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	match mode:
-		Mode.FadeIn:
-			content.modulate.a += delta / fadeInTime
-			if (content.modulate.a >= 1.0):
-				if !isItGameOver:
-					dayNightAnimation()
-				onFadeInFinished.emit()
-				mode = Mode.Visible
-	
-		Mode.FadeOut:
-			content.modulate.a -= delta / fadeOutTime
-			if (content.modulate.a <= 0.0):
-				onFadeOutFinished.emit()
-				queue_free()
+	if !isItGameOver or gameOverAnimFinished:
+		match mode:
+			Mode.FadeIn:
+				content.modulate.a += delta / fadeInTime
+				if (content.modulate.a >= 1.0):
+					if !isItGameOver:
+						dayNightAnimation()
+					onFadeInFinished.emit()
+					mode = Mode.Visible
+		
+			Mode.FadeOut:
+				content.modulate.a -= delta / fadeOutTime
+				if (content.modulate.a <= 0.0):
+					onFadeOutFinished.emit()
+					queue_free()
 
 func dayNightAnimation():
+	$CanvasLayer/Panel/BlackSprite.visible = true
 	toSprite.visible = true
 	animationPlayer.play("day_to_night_animation")
+	
+func gameOverAnimation():
+	animationPlayer.play("game_over_animation")
+	await get_tree().create_timer(0.3).timeout
+	bloodSprites1.visible = true
+	await get_tree().create_timer(0.4).timeout
+	bloodSprites2.visible = true
+	
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "day_to_night_animation":
 		fromSprite.visible = false
+	if anim_name == "game_over_animation":
+		gameOverAnimFinished = true
